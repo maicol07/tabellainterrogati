0b28d1d519c346890d9d1c6e324b2659ddb9ce18
Maicol <maicolbattistini@live.it>
2017-01-21 12:31:04 +0100

    Update tabella.php: ottimizzazione del codice
    *Ottimizzazione generale del codice: 
    - Aggiunta di commenti;
    - Aggiunto link di supporto
    - Cambiamenti minori
    Altre importanti novità in arrivo prossimamente...

--- tabella.php
+++ tabella.php
@@ -1,40 +1,36 @@
 <html>
+<head>
+<title>Tabella interrogati Pentamestre A.S. 2016/2017</title>
+</head>
 <body>
+<h1 align=center>Tabella interrogati Pentamestre A.S. 2016/2017<h1>
+<p><small>Grazie a questa tabella si potrà vedere chi è stato interrogato e così prevedere le future interrogazioni (utile nelle ore della Di Caprio).
+Per segnalazioni di errori e/o richieste scrivere qui: <a href="https://github.com/maicol07/tabellainterrogati/issues">Github Issues></a> oppure inviare una email a <a href="mailto:maicolbattistini@live.it">maicolbattistini@live.it></a></small></p>
 <table border="1">
-        <tr>
-                <td>Nome</td>
-                <td> - </td> 
-                <td>Cognome</td>
-                <td> - </td> 
-              <td>Italiano 1</td>
-                <td> - </td> 
-                <td>Storia 1</td>
-                <td> - </td> 
-                <td>Scienze 1</td>
-                <td> - </td>
-        </tr>
+        <tr>
+			// Inserire di seguito le colonne della tabella (queste sono i nomi visibili delle celle)
+                <td>N° REGISTRO</td>
+                <td>Nome e Cognome</td>
+              <td>Italiano Giro 1</td>
+                <td>Storia Giro 1</td>
+        </tr>
 <?php
-// Inserire i dati del database MySQL qui sotto
-$db_connection= mysql_connect(HOST,USER,PASSWORD);
-$db_selection = mysql_select_db(DB_NAME,$db_connection);
-
-$query = mysql_query("SELECT * FROM Interrogati");
+// Inserire i dati del database MySQL qui sotto (host,nomeutente,password);(database)
+$db_connection= mysql_connect("localhost","root","pass");
+$db_selection = mysql_select_db("tabellainterrogati",$db_connection);
+$query = mysql_query("SELECT * FROM Interrogati");
 while($cicle=mysql_fetch_array($query)){
-    echo " 
-         <tr>
-                <td>".$cicle['Nome']."</td>
-                <td> - </td> 
-                <td>".$cicle['Cognome']."</td> 
-                <td> - </td> 
-              <td>".$cicle['Ita1']."</td>
-                <td> - </td> 
-                <td>".$cicle['Sto1']."</td> 
-                <td> - </td> 
-                <td>".$cicle['Scie1']."</td>
-                <td> - </td> 
-          </tr>";
-         }
+    echo " 
+         <tr>
+			// Inserire i nomi delle colonne assegnate nella tabella del database. Queste corrisponderanno a quelle sopra
+                <td>".$cicle['ID']."</td>
+                <td>".$cicle['Nome_Cognome']."</td> 
+              <td>".$cicle['ITA1']."</td>
+                <td>".$cicle['STO1']."</td> 
+          </tr>";
+         }
 ?>
 </table>
+<p><small>Creatore: Battistini Maicol</small></p>
 </body>
 </html>
