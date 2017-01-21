80c9e2b6b2caa63fa55da6c833e18f16a0c112b2
Maicol <maicolbattistini@live.it>
2017-01-21 17:28:43 +0100

    Update tabella.php
    *Apportate modifiche minori:
    - Sfondo pagina cambiato
    - Modifiche ai font
    - Ottimizzata la codifica della pagina
    - Altre modifiche minori
    Novità presto in arrivo!!

--- tabella.php
+++ tabella.php
@@ -1,14 +1,18 @@
 <html>
 <head>
-<title>Tabella interrogati Pentamestre A.S. 2016/2017</title>
+<meta charset="utf-8" />
+<title>Tabella interrogati 2A Pentamestre A.S. 2016/2017</title>
 </head>
-<body>
-<h1 align=center>Tabella interrogati Pentamestre A.S. 2016/2017<h1>
-<p><small>Grazie a questa tabella si potrà vedere chi è stato interrogato e così prevedere le future interrogazioni (utile nelle ore della Di Caprio).
-Per segnalazioni di errori e/o richieste scrivere qui: <a href="https://github.com/maicol07/tabellainterrogati/issues">Github Issues></a> oppure inviare una email a <a href="mailto:maicolbattistini@live.it">maicolbattistini@live.it></a></small></p>
+<body style="color: rgb(0, 0, 0); background-color: rgb(255, 255, 204);"
+alink="#000099" link="#000099" vlink="#990099">
+<title>Tabella interrogati 2A Pentamestre A.S. 2016/2017</title>
+<h1 style="font-weight: bold; font-family: Century;" align="center">Tabella interrogati 2A Pentamestre A.S. 2016/2017</h1>
+<span style="font-family: Century;">Grazie a questa tabella si potrà vedere chi è stato interrogato e così prevedere le future interrogazioni (utile nelle ore della Di Caprio).
+Per segnalazioni di errori e/o richieste scrivere qui: </span><a style="font-family: Century;" href="https://github.com/maicol07/tabellainterrogati/issues">Github
+Issues </a><span style="font-family: Century;">oppure inviare una email a </span><a style="font-family: Century;" href="mailto:maicolbattistini@live.it">maicolbattistini@live.it</a>
 <table border="1">
         <tr>
-			// Inserire di seguito le colonne della tabella (queste sono i nomi visibili delle celle)
+			<!-- Inserire di seguito le colonne della tabella (queste sono i nomi visibili delle celle) -->
                 <td>N° REGISTRO</td>
                 <td>Nome e Cognome</td>
               <td>Italiano Giro 1</td>
@@ -16,8 +20,8 @@ Per segnalazioni di errori e/o richieste scrivere qui: <a href="https://github.c
         </tr>
 <?php
 // Inserire i dati del database MySQL qui sotto (host,nomeutente,password);(database)
-$db_connection= mysql_connect("localhost","root","pass");
-$db_selection = mysql_select_db("tabellainterrogati",$db_connection);
+$db_connection= mysql_connect("HOST,"USERNAME","PASSWORD");
+$db_selection = mysql_select_db("DB_NAME",$db_connection);
 $query = mysql_query("SELECT * FROM Interrogati");
 while($cicle=mysql_fetch_array($query)){
     echo " 
@@ -31,6 +35,6 @@ while($cicle=mysql_fetch_array($query)){
          }
 ?>
 </table>
-<p><small>Creatore: Battistini Maicol</small></p>
+<span style="font-family: Century;">Creatore: Battistini Maicol</small></span>
 </body>
 </html>
