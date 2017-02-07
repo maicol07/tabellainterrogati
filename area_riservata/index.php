2c47a2e6a4d20cc1f9047b85ae1134db1a1a599f
Maicol <maicolbattistini@live.it>
2017-02-07 21:19:50 +0100

    Create index.php
    Modulo di registrazione e login per l'area riservata.
    Funzioni:
    *Accesso
    *Registrazione

---
+++ area_riservata/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Area riservata | Tabella interrogati</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<div id="login"><div id="titolologin"><label>Accedi o registrati</label></div>
+<form action="privata.php" class="formlogin" method="post">
+<label>Nome utente:</label>
+<input type="text" name="username" class="testo" placeholder="Username..." /><br>
+<label>Password:</label>
+<input type="password" name="password" class="testo" placeholder="Password..." /><br>
+<a href="registrati.php"><input type="button" class="button" id="register" name="Register" value="Registrati" /></a>
+<input type="submit" class="button" name="submit" value="Accedi" />
+</form>
+</div>
+</body>
+</html>
