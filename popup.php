ee1bd9cfe14540ac31fa51afbe8bef93babdb0c6
Maicol <maicolbattistini@live.it>
2017-03-03 21:15:44 +0100

    Update popup.php
    *Aggiunto nuovo stile CSS al registro delle modifiche

--- popup.php
+++ popup.php
@@ -8,21 +8,104 @@
 <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 <!--Let browser know website is optimized for mobile-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<title>Tabella interrogati 2A Pentamestre A.S. 2016/2017</title>
+<title>Tabella interrogati | Changelog</title>
+<style type="text/css"  media="all">
+
+/* begin styles for RSS Feed */
+
+.rss-box {
+	 margin: 10px 5%;
+	 padding: 4px 8px;
+
+	 background-color: #ededed;
+	 border: 2px dashed #7485CA;
+}
+
+.rss-title, rss-title a {
+	font-family: "American Typewriter", "Trebuchet MS", Trebuchet, Lucida, sans-serif;
+	font-size: 18px;
+	font-weight:bold;
+	margin: 5px 0;
+	padding: 0;
+	letter-spacing: 1px;
+}
+
+.rss-items {
+  
+}
+
+.rss-item  {
+  font-family: verdana, arial, sans-serif;
+  font-size: 13px;
+  font-weight : bold; 
+  margin: 8px 0;
+}
+
+.rss-item a:link, .rss-item a:visited, .rss-item a:active {
+	text-decoration : none;
+	border-bottom: 1px solid #ededed;
+	color: #88b;
+	}
+
+.rss-item a:hover { 
+	text-decoration : none;
+	color: #e0861e;
+	border-bottom: 1px dotted #e0861e;
+	}
+	
+.rss-date {
+	font-size: 11px;
+	font-weight : normal;
+	color: #F60;
+	}
+	
+/* buttons modeled from http://www.wellstyled.com/css-inline-buttons.html */
+
+.pod-play {
+   _width:12em;
+   margin: 0 0.2em; padding: 0.1em 0; _padding:0;
+   
+   white-space:nowrap;
+   text-decoration: none;
+   vertical-align:middle;
+   background: #fb6;
+   color: black;
+   }
+.pod-play em {
+   _width:1em; _cursor:hand;
+   font-style: normal;
+   margin:0; padding: 0.1em 0.5em;
+   background: white;
+   color: #222;
+   }
+.pod-play span {
+   _width:1em; _cursor:hand;
+   margin:0; padding: 0.1em 0.5em 0.1em 0.3em;
+   }
+.pod-play:hover {
+   background: #666;
+   color: white;
+   }
+.pod-play:hover em {
+   background: black;
+   color: white
+   }
+
+</style>
 </head>
 <body style="color: rgb(0, 0, 0);"
 alink="#000099" link="#000099" vlink="#990099">
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="js/materialize.min.js"></script>
-<div style="margin-left:20px; margin-right:20px"> 
+<div style="margin-left:10px; margin-right:10px">
 <script language="JavaScript" src="http://feedroll.com/rssviewer/feed2js.php?src=https%3A%2F%2Fgithub.com%2Fmaicol07%2Ftabellainterrogati%2Freleases.atom&chan=y&utf=y&pc=y&html=a"  charset="UTF-8" type="text/javascript"></script>
 <noscript>
 <a href="http://feedroll.com/rssviewer/feed2js.php?src=https%3A%2F%2Fgithub.com%2Fmaicol07%2Ftabellainterrogati%2Freleases.atom&chan=y&utf=y&pc=y&html=y">Visualizza Changelog</a>
 </noscript>
-<div align="center">
-<a href="javascript:self.close()" class="btn waves-effect waves-light">Chiudi finestra</a>
-</div>
+<form>
+<input type="button" class="btn waves-effect waves-light" value="Chiudi Finestra" onClick="self.close()">
+</form>
 </div>
 </body>
 </html>
